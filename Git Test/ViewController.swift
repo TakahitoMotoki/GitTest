525a4e942f90122e2627a630fd4197cd0465e38f
元木嵩人 <kmnernofkingdeep@gmail.com>
2017-11-20 14:58:39 +0900

    First Commit

--- Git Test/ViewController.swift
+++ Git Test/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number: Int = 3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
